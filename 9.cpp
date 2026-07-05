2ed34729f94d47b777e1c9f98fdcebee749ffb17
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2026-07-05 07:45:17 +0530

    day 9

---
+++ 9.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s="suryansh";
+    int i=0;
+    int j =s.length()-1;
+    while(i<j){
+        swap(s[i], s[j]);
+        i++;
+        j--;
+    }
+    cout <<s;
+    return 0;
+}
